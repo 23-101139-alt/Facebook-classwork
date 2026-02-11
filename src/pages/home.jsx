3d5497221ecb024464177b211923b472708139dc
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-11 11:58:22 +0200

    structure

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './home.css';
+
+const Home = () => {
+    return ( 
+       <h1>facebook</h1>
+     );
+}
+ 
+export default Home;
