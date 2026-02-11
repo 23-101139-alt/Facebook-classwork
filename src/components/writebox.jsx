304cca36d6ad2af554487e4217a9da46eb33cbd4
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-11 15:58:22 +0200

    midd-box

---
+++ src/components/writebox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/writebox.css';
+
+const Writebox = () => {
+    return ( 
+
+        <div className='mainbox2-middele'>
+
+        </div>
+     );
+}
+ 
+export default Writebox;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './home.css';
+import Headerr from '../components/header';
+import Ctaa from '../components/button';
+import pic1 from '../assets/pic1.png';
+import Asidee from '../components/watch';
+import Buttonimgg from '../components/buttonimg';
+import Cardpic1 from '../components/cardpic';
 
 const Home = () => {
     return ( 
-       <h1>facebook</h1>
+        <>
+
+            <header className='headerr'>
+                    <img src={pic1} alt="header"  />
+                    <nav className='container-icons'>
+                     <Headerr />
+                     <Headerr />
+                     <Headerr />
+                     <Headerr />
+                     </nav>
+            </header>
+           <main className='mainn'>
+                <section className='left-bar'>
+                     <Ctaa />
+                     <aside className='iconplustext'>
+                        <Asidee />
+                        <Asidee />
+                        <Asidee />
+                        <Asidee />
+                     </aside>
+                        <div className='box1-part3-aside'>
+                            <h1 className='title-font'>Title</h1>
+                            <nav className='link-seemore-font' >See all</nav>
+                        </div>
+                        <button className='button-img-text-last-one-aside'>
+                            <Buttonimgg />
+                            <Buttonimgg />
+                            <Buttonimgg />
+                            <Buttonimgg />
+                            <Buttonimgg />
+                            <Buttonimgg />
+                        </button>
+
+                     <aside>
+
+                     </aside>
+                </section>
+
+                <section className='sec2'>
+                    <div className='box1-middle'>
+                        <Cardpic1 />
+                        <Cardpic1 />
+                        <Cardpic1 />
+                        <Cardpic1 />
+                    </div>
+                </section>
+
+
+
+           </main>
+
+        </>
+       
      );
 }
  
